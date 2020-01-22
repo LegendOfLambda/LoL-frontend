3ca1ec2ea44c00f26c9cc28a8356e7052645bc4d
justinandrade <lambda_school_loaner_202@Lambda-School-Loaner-202s-MacBook-Pro.local>
2020-01-21 23:53:43 -0500

    revert back

--- lol-frontend/src/App.tsx
+++ lol-frontend/src/App.tsx
@@ -1,24 +1,34 @@
-import React from "react";
-import "./App.scss";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  RouteComponentProps
-} from "react-router-dom";
-import World from "./components/world";
-import PlayerInformation from "./components/player-info";
-import { LandingPage, Game } from "./pages/index";
+import React from 'react';
+import './App.scss';
+import World from './components/world';
+import PlayerInformation from './components/player-info';
 
 const App: React.SFC<{}> = () => {
   return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/play-now" component={Game} />
-      </div>
-    </Router>
+    <div className="App">
+      <World tiles={[]}/>
+      <PlayerInformation
+        geo={{
+          position: [],
+          spriteLocation: '',
+          direction: '',
+          walkIndex: 0,
+          level: 0
+        }}
+        player={{
+          name: '',
+          hp: 5,
+          str: 10,
+          def: 10,
+          stamina: 10
+        }}
+        inventory={{
+          gems: 0,
+          max_slots: 6
+        }}
+      />
+    </div>
   );
-};
+}
 
 export default App;
--- lol-frontend/src/pages/LandingPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { RouteComponentProps } from "react-router";
-
-function LandingPage() {
-  return <div>Default</div>;
-}
-
-export default LandingPage;
