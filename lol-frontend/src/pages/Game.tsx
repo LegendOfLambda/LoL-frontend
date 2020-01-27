65dd1defe3ab1ffa2d358a47cc4933aba488de79
tbelk12 <taylor.belk@americannational.com>
2020-01-27 15:48:07 -0600

    finished output log

--- lol-frontend/src/pages/Game.tsx
+++ lol-frontend/src/pages/Game.tsx
@@ -2,10 +2,15 @@ import React from "react";
 
 import World from "../components/world";
 import PlayerInformation from "../components/player-info";
+import GameOuput from '../components/game-output';
+
+import { rooms } from '../maps/one';
+import './game.scss';
 
 function Game() {
   return (
     <div className="game">
+      <div className='left-side'>
       <World room={{
           title: 'Dusty Dungeon Entry',
           description: 'You have entered the dusty dungeon!',
@@ -35,6 +40,11 @@ function Game() {
           max_slots: 6
         }}
       />
+      </div>
+
+      <div className='game-ouput'>
+        <GameOuput map={rooms[0]}/>
+      </div>
     </div>
   );
 }
