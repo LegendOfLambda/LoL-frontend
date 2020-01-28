cd4a1dc4db4d5edfe7d61a7b3bda27b5cdeedf5f
justinandrade <j.andrade.john@gmail.com>
2020-01-28 03:45:55 -0500

    boilerplate added for story and features page

---
+++ lol-frontend/src/pages/Features/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../../components/navigation";
+
+import "./features.scss";
+
+function Features() {
+  return (
+    <div className="Features-page">
+      <NavBar />
+      <h2>Features</h2>
+    </div>
+  );
+}
+
+export default Features;
---
+++ lol-frontend/src/pages/Story/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../../components/navigation";
+
+import "./story.scss";
+
+function Story() {
+  return (
+    <div className="story-page">
+      <NavBar />
+      <h2>Story</h2>
+    </div>
+  );
+}
+
+export default Story;
