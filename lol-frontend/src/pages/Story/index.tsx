60c9698f5abfb3eab7327666aa3eaf53a8d3a16f
Justin Andrade <42485697+JustinAndrade@users.noreply.github.com>
2020-07-06 04:22:59 -0400

    Revert "Revert "New layout and styling added.""

---
+++ lol-frontend/src/pages/Story/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../../components/navigation";
+
+import "./story.scss";
+
+function Story() {
+  return (
+    <div className="story-page">
+      <NavBar />
+      <h2>Coming soon . . .</h2>
+    </div>
+  );
+}
+
+export default Story;
